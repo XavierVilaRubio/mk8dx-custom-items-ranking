21b8377a14fcd749a92d7f5b4ca790b905b97d68
Xavier Vila <xvrvilarubio2001@gmail.com>
2024-04-06 17:19:57 +0200

    add responsive styles to header

--- app.vue
+++ app.vue
@@ -5,11 +5,11 @@ import { Plus } from 'lucide-vue-next'
 const { data: customItems, refresh } = await useFetch('/api/custom_items')
 </script>
 <template>
-  <div class="container pt-8 max-w-6xl">
-    <div class="flex gap-2 justify-between mb-8">
+  <div class="container pt-8 max-w-6xl @container">
+    <div class="flex gap-2 justify-between mb-8 @lg:flex-row flex-col">
       <h1 class="text-2xl font-semibold">MK8DX Custom Items Ranking</h1>
       <SelectCustomItemsModal @customItemsAdded="refresh">
-        <Button variant="outline"
+        <Button variant="outline" class="self-end w-fit"
           ><Plus class="w-4 h-4 mr-2" :stroke-width="3" /> Create Custom
           Items</Button
         >
