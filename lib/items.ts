bd13d9fe1a79d00e236806be3fc193e4b925f0b7
Xavier Vila <xvrvilarubio2001@gmail.com>
2024-04-06 15:46:52 +0200

    organize assets

---
+++ lib/items.ts
@@ -0,0 +1,113 @@
+import StarMK8 from '~/assets/img/items/StarMK8.png'
+import SuperHornMK8 from '~/assets/img/items/SuperHornMK8.png'
+import TripleBananaMK8 from '~/assets/img/items/TripleBananaMK8.png'
+import GoldenMushroomMK8 from '~/assets/img/items/GoldenMushroomMK8.png'
+import BulletBillMK8 from '~/assets/img/items/BulletBillMK8.png'
+import CoinMK8 from '~/assets/img/items/CoinMK8.png'
+import LightningBoltMK8 from '~/assets/img/items/LightningBoltMK8.png'
+import TripleRedShellsMK8 from '~/assets/img/items/TripleRedShellsMK8.png'
+import MushroomMK8 from '~/assets/img/items/MushroomMK8.png'
+import Crazy8MK8 from '~/assets/img/items/Crazy8MK8.png'
+import RedShellMK8 from '~/assets/img/items/RedShellMK8.png'
+import TripleGreenShellsMK8 from '~/assets/img/items/TripleGreenShellsMK8.png'
+import BoomerangFlowerMK8 from '~/assets/img/items/BoomerangFlowerMK8.png'
+import TripleMushroomMK8 from '~/assets/img/items/TripleMushroomMK8.png'
+import SpinyShellMK8 from '~/assets/img/items/SpinyShellMK8.png'
+import BlooperMK8 from '~/assets/img/items/BlooperMK8.png'
+import PiranhaPlantPotMK8 from '~/assets/img/items/PiranhaPlantPotMK8.png'
+import FireFlowerMK8 from '~/assets/img/items/FireFlowerMK8.png'
+import BobOmbMK8 from '~/assets/img/items/BobOmbMK8.png'
+import BananaMK8 from '~/assets/img/items/BananaMK8.png'
+import GreenShellMK8 from '~/assets/img/items/GreenShellMK8.png'
+import BooMK8 from '~/assets/img/items/BooMK8.png'
+
+export const items = [
+  {
+    img: BananaMK8,
+    name: 'banana',
+  },
+  {
+    img: TripleBananaMK8,
+    name: 'triple_banana',
+  },
+  {
+    img: GreenShellMK8,
+    name: 'green_shell',
+  },
+  {
+    img: TripleGreenShellsMK8,
+    name: 'triple_green_shells',
+  },
+  {
+    img: RedShellMK8,
+    name: 'red_shell',
+  },
+  {
+    img: TripleRedShellsMK8,
+    name: 'triple_red_shells',
+  },
+  {
+    img: SpinyShellMK8,
+    name: 'spiny_shell',
+  },
+  {
+    img: BobOmbMK8,
+    name: 'bob_omb',
+  },
+  {
+    img: MushroomMK8,
+    name: 'mushroom',
+  },
+  {
+    img: TripleMushroomMK8,
+    name: 'triple_mushroom',
+  },
+  {
+    img: GoldenMushroomMK8,
+    name: 'golden_mushroom',
+  },
+  {
+    img: BulletBillMK8,
+    name: 'bullet_bill',
+  },
+  {
+    img: BlooperMK8,
+    name: 'blooper',
+  },
+  {
+    img: LightningBoltMK8,
+    name: 'lightning_bolt',
+  },
+  {
+    img: StarMK8,
+    name: 'star',
+  },
+  {
+    img: FireFlowerMK8,
+    name: 'fire_flower',
+  },
+  {
+    img: BoomerangFlowerMK8,
+    name: 'boomerang_flower',
+  },
+  {
+    img: PiranhaPlantPotMK8,
+    name: 'piranha_plant_pot',
+  },
+  {
+    img: SuperHornMK8,
+    name: 'super_horn',
+  },
+  {
+    img: Crazy8MK8,
+    name: 'crazy8',
+  },
+  {
+    img: CoinMK8,
+    name: 'coin',
+  },
+  {
+    img: BooMK8,
+    name: 'boo',
+  },
+]
